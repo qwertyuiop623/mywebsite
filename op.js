a7f088f36720dc1acccd8bb0d8d4430629975086
qwertyuiop623 <73392005+qwertyuiop623@users.noreply.github.com>
2020-10-24 18:47:36 +0530

    Add files via upload

---
+++ op.js
@@ -0,0 +1,10 @@
+function conformation(){
+    var ans = confirm ("you want to see the website click ok button")
+    if (ans){
+        alert("welcome to KiNg")
+        window.location="file:///G:/coading/my%20info/index.html";
+    }
+    else{
+        alert("sorry! you click on cancle button")
+    }
+}
